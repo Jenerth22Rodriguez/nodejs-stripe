ea08f82340d85cd963b23f469edeb38a427ab9da
Jenerth22Rodriguez <Connerjamestill@gmail.com>
2025-06-22 01:12:52 -0400

    Adding stripe Functions and API

--- src/controllers/payment.controllers.js
+++ src/controllers/payment.controllers.js
@@ -1,1 +1,42 @@
-export const createSession = (req, res) => res.send("checkout");
+import Stripe from "stripe";
+
+const stripe = new Stripe();
+
+export const createSession = async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            product_data: {
+              name: "Laptop",
+              description: "Surface Pro",
+            },
+            currency: "usd",
+            unit_amount: 20000, // 200 dollars
+          },
+          quantity: 1,
+        },
+        {
+          price_data: {
+            product_data: {
+              name: "Orange Juice",
+              description: "with pulp",
+            },
+            currency: "usd",
+            unit_amount: 200, // 2 dollars
+          },
+          quantity: 3,
+        },
+      ],
+      mode: "payment",
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+    });
+
+    return res.json(session);
+  } catch (error) {
+    console.error("Stripe error:", error.message);
+    return res.status(500).json({ error: error.message });
+  }
+};
